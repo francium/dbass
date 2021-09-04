43d6d7969c16a399385facedaa37489d20f798f7
francium <francium@francium.cc>
2021-09-04 12:54:43 -0400

    initial commit

---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    printWidth: 90,
+    tabWidth: 4,
+    useTabs: false,
+    semi: true,
+    singleQuote: false,
+    quoteProps: "as-needed",
+    jsxSingleQuote: false,
+    trailingComma: "all",
+    bracketSpacing: false,
+    jsxBracketSameLine: false,
+    arrowParens: "avoid",
+    endOfLine: "lf",
+};
